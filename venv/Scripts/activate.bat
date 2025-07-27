b55fe86c9d82bae4152c55203d721d9d1b3d6eff
Rose-Alice18 <roselinetsatsu@gmail.com>
2025-07-27 18:28:33 +0000

    Numpy issue solved

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\Lylu_Summary_Agent\venv
+set VIRTUAL_ENV=C:\Users\HP\OneDrive\Desktop\Lylu_Summary_Agent\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
